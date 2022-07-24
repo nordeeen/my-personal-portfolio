31c30adfb509e44e4bc5a0af45bd6139fa42ac76
nordeeen <ahmadnrdnsah@gmail.com>
2022-07-24 21:00:09 +0700

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+  );
+};
+
+export default Navbar;
