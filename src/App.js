d0237dafd79c32d6cadd6c9be322959800b52a81
nordeeen <ahmadnrdnsah@gmail.com>
2022-07-29 06:19:34 +0700

    adding Work component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   );
 }
