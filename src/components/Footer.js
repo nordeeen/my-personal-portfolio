d2bd386b0bf05f79523b66b455355db2a3a821d2
nordeeen <ahmadnrdnsah@gmail.com>
2022-08-27 21:34:52 +0700

    add styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Logo from "../assets/mylogo.png";
 
 const Footer = () => {
-  return <div className="w-full h-[100px] bg-[red]">Footer</div>;
+  return (
+    <>
+      <footer className="w-full h-[100px] bg-[#0a192f] flex justify-center align-center">
+        <div className="flex flex-col">
+          <div className="px-16">
+            <img style={{ width: "75px" }} src={Logo} alt="logo" />
+          </div>
+          <h2 className="text-sm text-gray-300 font-bold">
+            Copyright &copy; Ahmad Nurdiansah
+          </h2>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
