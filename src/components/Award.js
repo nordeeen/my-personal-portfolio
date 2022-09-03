bf748a8af1cc4ce9dff3d34410b29957cd23e53a
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-03 19:15:38 +0700

    added styles

--- src/components/Award.js
+++ src/components/Award.js
@@ -57,61 +57,65 @@ const Award = () => {
     },
   ];
 
+  // flex flex-col justify-center items-center w-full h-full
   return (
     <>
       <section className="w-full h-full bg-[#0a192f] text-gray-300">
-        <div className="max-w-[1000px] mx-auto bg-[#0a192f] p-4 flex flex-col justify-center w-full h-full">
-          <div className="mb-3">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-              Award
-            </p>
-          </div>
-          <a
-            className="flex text-xl text-blue-600 underline font-semibold italic"
-            href="https://drive.google.com/drive/folders/1u2haQlTu7XO7Z456m_5RSId7jcK_itxg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={Link}
-              alt="logo-link"
-              style={{
-                width: "18px",
-                height: "18px",
-                objectFit: "cover",
-                marginRight: "11px",
-                marginTop: "5px",
-              }}
-            />
-            Certificate
-          </a>
-          <div className="mx-auto my-auto">
-            {datas.map((data) => {
-              return (
-                <div
-                  key={data.id}
-                  className="flex flex-col items-center bg-[#0a192f] rounded-lg border md:flex-row md:max-w-xl shadow-[#040c16] mb-4 shadow-md border-[#0a192f]"
-                >
-                  <img
-                    className="object-cover w-full h-100 rounded-t-lg md:h-auto md:w-60 md:rounded-none md:rounded-l-lg px-4 py-2"
-                    src={data.photo}
-                    alt="logo"
-                  />
-                  <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h1 className="mb-2 text-2xl font-bold tracking-tight text-pink-600 dark:text-white">
-                      {data.title}
-                    </h1>
-                    <p className="mb-3 font-bold text-gray-300 ">
-                      {data.description}
-                    </p>
-                    <p className="mb-3 font-bold text-gray-300 ">
-                      {data.skill} <span className="text-pink-600">&#187;</span>{" "}
-                      {data.year}
-                    </p>
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <div className="max-w-[1000px] mx-auto bg-[#0a192f] p-4 flex flex-col justify-center w-full h-full">
+            <div className="mb-3">
+              <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+                Award
+              </p>
+            </div>
+            <a
+              className="flex text-xl text-blue-600 underline font-semibold italic"
+              href="https://drive.google.com/drive/folders/1u2haQlTu7XO7Z456m_5RSId7jcK_itxg"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={Link}
+                alt="logo-link"
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  objectFit: "cover",
+                  marginRight: "11px",
+                  marginTop: "5px",
+                }}
+              />
+              Certificate
+            </a>
+            <div className="mx-auto my-auto">
+              {datas.map((data) => {
+                return (
+                  <div
+                    key={data.id}
+                    className="flex flex-col items-center bg-[#0a192f] rounded-lg border md:flex-row md:max-w-xl shadow-[#040c16] mb-4 shadow-md border-[#0a192f]"
+                  >
+                    <img
+                      className="object-cover w-full h-100 rounded-t-lg md:h-auto md:w-60 md:rounded-none md:rounded-l-lg px-4 py-2"
+                      src={data.photo}
+                      alt="logo"
+                    />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                      <h1 className="mb-2 text-2xl font-bold tracking-tight text-pink-600 dark:text-white">
+                        {data.title}
+                      </h1>
+                      <p className="mb-3 font-bold text-gray-300 ">
+                        {data.description}
+                      </p>
+                      <p className="mb-3 font-bold text-gray-300 ">
+                        {data.skill}{" "}
+                        <span className="text-pink-600">&#187;</span>{" "}
+                        {data.year}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </section>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -56,23 +56,25 @@ const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-            Skills
-          </p>
-          <p className="py-4">These are the technologies I've worked with</p>
-        </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          {dataSkill.map((item) => (
-            <div
-              key={item.id}
-              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
-            >
-              <img className="w-20 mx-auto" src={item.logo} alt="HTML icon" />
-              <p className="my-4 uppercase">{item.name}</p>
-            </div>
-          ))}
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+          <div>
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              Skills
+            </p>
+            <p className="py-4">These are the technologies I've worked with</p>
+          </div>
+          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+            {dataSkill.map((item) => (
+              <div
+                key={item.id}
+                className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+              >
+                <img className="w-20 mx-auto" src={item.logo} alt="HTML icon" />
+                <p className="my-4 uppercase">{item.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
