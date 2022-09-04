367f71fa610cb525b755088bc1d3db1326543079
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-04 21:01:52 +0700

    fixed navbar comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import mylogo from "../assets/mylogo.png";
 import { Link } from "react-scroll";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
