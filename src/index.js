215a78e87eeb00e491baa96d37dcb90b423badb4
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-04 15:46:59 +0700

    added minify css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
+import "./minify.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
