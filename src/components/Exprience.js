22b908050430aa9aa437e4ff6ebf0e980aac3645
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-04 16:07:05 +0700

    change name

--- src/components/Exprience.js
+++ src/components/Exprience.js
@@ -11,7 +11,7 @@ const Exprience = () => {
           <div className="max-w-[1000px] mx-auto bg-[#0a192f] p-4 flex flex-col justify-center w-full h-full">
             <div className="mb-12">
               <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-                Experience
+                Experiences
               </p>
             </div>
             <div
