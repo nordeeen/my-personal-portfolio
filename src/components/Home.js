7eadcd0941a4e4b439ab72253acceb980c7d71b6
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-05 16:22:40 +0700

    adding scroll down to Work component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -23,7 +24,9 @@ const Home = () => {
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
             <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
+              <Link to="work" smooth={true} duration={500}>
+                <HiArrowNarrowRight className="ml-3" />
+              </Link>
             </span>
           </button>
         </div>
