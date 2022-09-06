585d71a68081e07161576124284d4e336a7d058f
nordeeen <ahmadnrdnsah@gmail.com>
2022-09-06 19:47:30 +0700

    adding marginTop to wrapper Skill component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -58,7 +58,7 @@ const Skills = () => {
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full mt-2">
           <div>
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               Skills
