42fb8a99815a3ef7514aa065b1327e39fa51a613
nordeeen <ahmadnrdnsah@gmail.com>
2023-01-08 23:44:43 +0700

    added list project

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,8 @@ import React from "react";
 import foodOrder from "../assets/food-order.png";
 import natoursTravel from "../assets/natours-travel.png";
 import eplWeb from '../assets/epl-web.png';
+import laslesVPN from '../assets/lp-laslesvpn.png';
+import userLogin from '../assets/users-login.png';
 import Project from "./Project";
 
 const projects = [
@@ -25,6 +27,20 @@ const projects = [
     imgBg: eplWeb,
     linkPort: 'https://gibol.netlify.app/',
     linkGithub: 'https://github.com/nordeeen/Gibol-web-app',
+  },
+  {
+    id: 4,
+    title: 'Landing Page LaslesVPN',
+    imgBg: laslesVPN,
+    linkPort: 'https://landingpage-lasles-vpn.vercel.app/',
+    linkGithub: 'https://github.com/nordeeen/Landingpage-LaslesVPN',
+  },
+  {
+    id: 5,
+    title: 'Users Login Page',
+    imgBg: userLogin,
+    linkPort: 'https://users-login-web.vercel.app/',
+    linkGithub: 'https://github.com/nordeeen/Users-login-web',
   },
 ];
 
